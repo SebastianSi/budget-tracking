8e3810400f94133ec6e29a1bfe6abbe8cbd8a9b8
Andrei Sebastian Sirb <sebastian.sirb@clj-mb-008.3pillar.corp>
2018-04-05 11:28:37 +0300

    some adjustments

--- src/common/Modal.js
+++ src/common/Modal.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {Modal, Text, TouchableHighlight, View, TextInput} from 'react-native'
+// import FloatLabelTextInput from 'react-native-floating-label-text-input'
+import InlineLabelExample from './InlineLabelExample'
 
 export default class ModalExample extends Component {
     state = {
@@ -18,13 +20,11 @@ export default class ModalExample extends Component {
             height: 240,
             width: 280,
             backgroundColor: '#D3D3D3',
-            borderRadius: 22,
-            justifyContent: 'center',
-            alignItems: 'center'
+            borderRadius: 22
         }
 
         return (
-            <View style={{marginTop: 22, justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{marginTop: 22}}>
                 <Modal
                     animationType='fade'
                     transparent={true}
@@ -40,18 +40,39 @@ export default class ModalExample extends Component {
                                     this.setModalVisible(!this.state.modalVisible)
                                 }}>
                                 <View>
-                                    <Text>X</Text>
-                                    <Text>Item</Text>
-                                    <TextInput
-                                        style={{height: 30, width: 100, borderColor: 'black', borderWidth: 1, backgroundColor: '#fff'}}
-                                        onChangeText={(itemText) => this.setState({itemText})}
-                                        value={this.state.itemText}
-                                    />
+                                    <Text style={{alignSelf: 'flex-end', paddingRight:10, paddingTop: 10, marginBottom: 20}}>
+                                        x</Text>
+                                    <View style={{flexDirection:'row', flexWrap:'wrap', paddingBottom: 20}}>
+                                        <Text style={{alignSelf: 'flex-start', paddingRight:10}}>Item</Text>
+                                        <TextInput
+                                            style={{alignSelf: 'center', height: 30, width: 150, borderColor: 'grey', borderWidth: 0, backgroundColor: '#fff'}}
+                                            onChangeText={(itemText) => this.setState({itemText})}
+                                            value={this.state.itemText}
+                                        />
+                                    </View>
+                                    <View style={{flexDirection:'row', flexWrap:'wrap', paddingBottom: 20}}>
+                                        <Text style={{alignSelf: 'flex-start', paddingRight:10}}>Amount</Text>
+                                        <TextInput
+                                            style={{alignSelf: 'center', height: 30, width: 150, borderColor: 'grey', borderWidth: 0, backgroundColor: '#fff'}}
+                                            onChangeText={(itemText) => this.setState({itemText})}
+                                            value={this.state.itemText}
+                                        />
+                                    </View>
+                                    <View style={{flexDirection:'row', flexWrap:'wrap', paddingBottom: 20}}>
+                                        <Text style={{alignSelf: 'flex-start', paddingRight:10}}>Category</Text>
+                                        <TextInput
+                                            style={{alignSelf: 'center', height: 30, width: 150, borderColor: 'grey', borderWidth: 0, backgroundColor: '#fff'}}
+                                            onChangeText={(itemText) => this.setState({itemText})}
+                                            value={this.state.itemText}
+                                        />
+                                    </View>
                                 </View>
                             </TouchableHighlight>
-                            <Text></Text>
+                            {/*<Text></Text>*/}
                         </View>
                     </View>
+                    {/*<InlineLabelExample>*/}
+                    {/*</InlineLabelExample>*/}
                 </Modal>
 
                 <TouchableHighlight
