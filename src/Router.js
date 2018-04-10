cc6d76b3d0b642c9141bc14bcc1b9590e454cd13
sebastian.sirb <sebastian.sirb90@gmail.com>
2018-04-10 10:18:31 +0300

    replaced react-native-router-flux with react-navigation

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
-import Home from './home/Home'
-import RecordsContainer from './records/RecordsContainer'
-
-const RouterComponent = () => {
-    return (
-        <Router sceneStyle={{ paddingTop: 1 }}>
-            <Scene>
-                <Scene key="home" component={Home} title="Home" initial/>
-                <Scene key="recordsContainer" component={RecordsContainer} title="Records Container"/>
-            </Scene>
-        </Router>
-    )
-}
-
-export default RouterComponent
+// import React from 'react'
+// import { Scene, Router } from 'react-native-router-flux'
+// import Home from './home/Home'
+// import RecordsContainer from './records/RecordsContainer'
+//
+// const RouterComponent = () => {
+//     return (
+//         <Router sceneStyle={{ paddingTop: 1 }}>
+//             <Scene>
+//                 <Scene key="home" component={Home} title="Home" initial/>
+//                 <Scene key="recordsContainer" component={RecordsContainer} title="Records Container"/>
+//             </Scene>
+//         </Router>
+//     )
+// }
+//
+// export default RouterComponent
