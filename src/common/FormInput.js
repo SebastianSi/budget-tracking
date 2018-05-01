f24f26a06bf73e82c31b112a509851035e384925
sebastian.sirb <sebastian.sirb90@gmail.com>
2018-05-01 14:26:04 +0300

    switched colors a bit pand stored categories in local storage

--- src/common/FormInput.js
+++ src/common/FormInput.js
@@ -5,10 +5,11 @@ const FormInput = (props) => {
 
     let { distanceToLabel } = props
     return (
-        <View style={{flexDirection:'row', flexWrap:'wrap', paddingBottom: 20}}>
+        <View style={{flexDirection:'row', flexWrap:'wrap', paddingBottom: 24}}>
             <Text style={{alignSelf: 'flex-start', paddingRight:distanceToLabel, paddingLeft: 10}}>{props.labelText}</Text>
             <TextInput
-                style={{alignSelf: 'center', height: 30, width: 150, borderColor: 'grey', borderWidth: 0, backgroundColor: '#fff'}}
+                style={{alignSelf: 'center', height: 33, width: 150, borderColor: 'grey', borderWidth: 0,
+                    backgroundColor: '#212121', color: '#fff', fontWeight: '800'}}
                 onChangeText={(qtyVal) => props.setFormState(qtyVal)}
                 value={props.qtyVal}
             />
